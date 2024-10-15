b731383a5b8590af5dce8275b684c8037089ee13
Robin Ung <Robin_ung@hotmail.com>
2024-10-15 18:45:13 +0000

    Add validation email, username is unique

--- users/serializers.py
+++ users/serializers.py
@@ -1,12 +1,19 @@
 from rest_framework import serializers
 from dj_rest_auth.registration.serializers import RegisterSerializer
+from rest_framework.validators import UniqueValidator
 from allauth.account.adapter import get_adapter
 from allauth.account.utils import setup_user_email
 from users.models import User
 
 class CustomRegisterSerializer(RegisterSerializer):
-    email = serializers.EmailField(required=True)
-    username = serializers.CharField(required=True)
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    username = serializers.CharField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
 
     def get_cleaned_data(self):
         data = super().get_cleaned_data()
@@ -18,10 +25,11 @@ class CustomRegisterSerializer(RegisterSerializer):
         adapter = get_adapter()
         user = adapter.new_user(request)
         self.cleaned_data = self.get_cleaned_data()
+        
         user.email = self.cleaned_data.get('email')
         user.username = self.cleaned_data.get('username')
         user.save()
         setup_user_email(request, user, [])
+
         adapter.save_user(request, user, self)
         return user
-
