ccc2e2f3179cf33f35c947db68f7de9cc2f7ebd6
Robin Ung <Robin_ung@hotmail.com>
2024-10-15 18:44:15 +0000

    Add error if no username, Change username length 155 > 55

--- users/models.py
+++ users/models.py
@@ -1,10 +1,14 @@
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
+# Custom User Manager
 class UserManager(BaseUserManager):
     def create_user(self, email, username, password=None, **extra_fields):
         if not email:
             raise ValueError("The Email field must be set")
+        if not username:
+            raise ValueError("The Username field must be set")
+        
         email = self.normalize_email(email)
         user = self.model(email=email, username=username, **extra_fields)
         user.set_password(password)
@@ -22,17 +26,21 @@ class UserManager(BaseUserManager):
 
         return self.create_user(email, username, password, **extra_fields)
 
-
+# Custom User Model
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
-    username = models.CharField(max_length=255, unique=True)
+    username = models.CharField(max_length=55, unique=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
+    # Link to the custom user manager
     objects = UserManager()
 
+    # Field that will be used for login
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
     def __str__(self):
         return self.email
+
+    
