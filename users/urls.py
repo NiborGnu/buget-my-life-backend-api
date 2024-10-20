79135766543e394ce552ba87498b97fb5536d414
Robin Ung <Robin_ung@hotmail.com>
2024-10-20 15:10:30 +0000

    Change urls path

--- users/urls.py
+++ users/urls.py
@@ -2,11 +2,11 @@ from django.urls import path, include
 
 urlpatterns = [
     path(
-        'auth/',
+        '',
         include('dj_rest_auth.urls')
     ),
     path(
-        'auth/registration/',
+        'registration/',
         include('dj_rest_auth.registration.urls')
     ),
 ]
