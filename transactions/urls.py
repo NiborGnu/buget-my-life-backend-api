53ed3e50d58fcd3c8ba4e1ccbbe19a248e511596
Robin Ung <Robin_ung@hotmail.com>
2024-10-20 15:15:25 +0000

    Created urls transaction using rest_framework.router

---
+++ transactions/urls.py
@@ -0,0 +1,11 @@
+from rest_framework.routers import DefaultRouter
+from .views import TransactionViewSet
+
+router = DefaultRouter()
+router.register(
+    '', 
+    TransactionViewSet, 
+    basename='transactions'
+)
+
+urlpatterns = router.urls
