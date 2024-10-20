1aee330c609279278e3f2f9d9cfbaff301aee8ec
Robin Ung <Robin_ung@hotmail.com>
2024-10-20 21:29:00 +0000

    Add comments

--- transactions/urls.py
+++ transactions/urls.py
@@ -1,11 +1,16 @@
 from rest_framework.routers import DefaultRouter
-from .views import TransactionViewSet
+from .views import TransactionViewSet, TransactionCommentViewSet
 
 router = DefaultRouter()
 router.register(
     '', 
     TransactionViewSet, 
     basename='transactions'
+)
+router.register(
+    'comments',
+    TransactionCommentViewSet,
+    basename='comments'
 )
 
 urlpatterns = router.urls
