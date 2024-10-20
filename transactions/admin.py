00406ca45c833133bd5453a7499938f842a2d7b6
Robin Ung <Robin_ung@hotmail.com>
2024-10-20 21:28:20 +0000

    Add admin class transaction and comments

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Transaction, TransactionComment
 
-# Register your models here.
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'amount', 'transaction_type', 'category', 'created_at')
+    search_fields = ('user__username', 'amount', 'transaction_type', 'category__name')
+    list_filter = ('transaction_type', 'category')
+
+class TransactionCommentAdmin(admin.ModelAdmin):
+    list_display = ('transaction', 'user', 'comment', 'created_at')
+    search_fields = ('transaction__description', 'user__username', 'comment')
+
+admin.site.register(Transaction, TransactionAdmin)
+admin.site.register(TransactionComment, TransactionCommentAdmin)
