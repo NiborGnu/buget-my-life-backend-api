53e06dd61f332b25084b1fc06d84eb78fa3a4b9c
Robin Ung <Robin_ung@hotmail.com>
2024-10-22 21:39:31 +0000

    Add class GoalViewSet

--- goals/views.py
+++ goals/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Goal
+from .serializers import GoalSerializer
+
+class GoalViewSet(viewsets.ModelViewSet):
+    serializer_class = GoalSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # Return only the goals belonging to the authenticated user
+        return Goal.objects.filter(user=self.request.user)
